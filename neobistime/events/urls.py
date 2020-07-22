e2e8c27f7ef06824dd6448fe2ea7e264e64835dc
inbraev <ibraev.nazar@yandex.ru>
2020-07-23 04:19:41 +0600

    added my polls, retrieve and update for poll

--- neobistime/events/urls.py
+++ neobistime/events/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('event/<int:pk>/', views.EventDetail().as_view(),name='detail-event'),
     path('place/', views.PlaceListCreateView().as_view(),name='places'),
     path('poll/', views.PollCreateView().as_view(),name='create-poll'),
+    path('poll/<int:pk>/', views.PollDetailView().as_view(),name='detail-poll'),
+    path('my_poll/', views.MyPollListView().as_view(),name='my-polls'),
 
 ]
