9b87287e6625c0199c0204806fcc2eb9c2eb7c84
inbraev <ibraev.nazar@yandex.ru>
2020-07-25 20:52:18 +0600

    finished email notification

--- neobistime/events/views.py
+++ neobistime/events/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics, status
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.response import Response
 from .serializers import *
 from rest_framework import permissions
 from .permissions import *
