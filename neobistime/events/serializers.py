d47b554bc4cbeb8057d3f43a40fa71c36ea4dcca
inbraev <ibraev.nazar@yandex.ru>
2020-07-28 09:03:28 +0600

    added blocking functionality if event violates someone's schedule

--- neobistime/events/serializers.py
+++ neobistime/events/serializers.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 from django.utils import timezone
 from .models import *
+from django.db.models import Q
 
 
 class PlaceSerializer(serializers.ModelSerializer):
@@ -30,7 +31,7 @@ class PollSerializer(serializers.ModelSerializer):
         """
         # Check that event created by admins, otherwise
         # return error because usual users can't create polls
-        if data['event'].owner.is_staff():
+        if data['event'].owner.is_staff:
             if data['event'].deadline < timezone.now():
                 raise serializers.ValidationError("Вы пропустили дедлайн!!!")
             return data
@@ -43,13 +44,14 @@ class PollRetrieveUpdateSerializer(serializers.ModelSerializer):
     Class for retrieving and updating poll instance.
     """
     event = serializers.ReadOnlyField(source='event.title')
+    address = serializers.ReadOnlyField(source='event.address')
     place = PlaceSerializer(read_only=True, source='event.place')
     start_date = serializers.ReadOnlyField(source='event.start_date')
     end_date = serializers.ReadOnlyField(source='event.end_date')
 
     class Meta:
         model = Poll
-        fields = ('id', 'answer', 'rejection_reason', 'event', 'place', 'start_date', 'end_date')
+        fields = ('id', 'answer', 'rejection_reason', 'event', 'address', 'place', 'start_date', 'end_date')
 
 
 class EventGetSerializer(serializers.ModelSerializer):
@@ -85,6 +87,32 @@ class EventGetSerializer(serializers.ModelSerializer):
             return 'blue'
 
 
+def available_date_for_event(validated_data):
+    """
+    Checking if event violates someone's scheduled event
+    :param validated_data:
+    :return: information of conflict event
+    """
+    start = validated_data['start_date']
+    end = validated_data['end_date']
+    # list contains events that already exist in this time
+    event = []
+    filter_params = dict(start_date__lte=end, end_date__gte=start)
+    # only checking events that take place in the office
+    if 'Маленькая' in validated_data['place'].name:
+        event = Event.objects.filter(Q(place__name='Маленькая комната') | Q(place__name='Весь офис'), **filter_params)
+    elif 'Большая' in validated_data['place'].name:
+        event = Event.objects.filter(Q(place__name='Большая комната') | Q(place__name='Весь офис'), **filter_params)
+    elif 'Весь' in validated_data['place'].name:
+        event = Event.objects.filter(**filter_params).exclude(place__name='Другое')
+    if event:
+        raise serializers.ValidationError({"Error": "place is not empty",
+                                           "Event title": event[0].title,
+                                           "Place": event[0].place.name,
+                                           "start": event[0].start_date,
+                                           "end": event[0].end_date})
+
+
 class EventCreateUpdateSerializer(serializers.ModelSerializer):
     """
          Class for serializing Event models for post and update methods
@@ -96,7 +124,12 @@ class EventCreateUpdateSerializer(serializers.ModelSerializer):
         model = Event
         fields = (
             'id', 'owner', 'title', 'description', 'deadline', 'start_date', 'end_date', 'place', 'link', 'address',
-            )
+        )
+
+    def create(self, validated_data):
+        available_date_for_event(validated_data)
+
+        return Event.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
@@ -107,5 +140,6 @@ class EventCreateUpdateSerializer(serializers.ModelSerializer):
         instance.end_date = validated_data.get('end_date', instance.end_date)
         instance.deadline = validated_data.get('deadline', instance.deadline)
         instance.link = validated_data.get('link', instance.link)
+        available_date_for_event(validated_data)
         instance.save()
         return instance
