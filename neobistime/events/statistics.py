00ceae7ec7523c22fc1c82cd758f0a5458035f3d
inbraev <ibraev.nazar@yandex.ru>
2020-08-05 04:11:40 +0600

    refactored code in statistic

--- neobistime/events/statistics.py
+++ neobistime/events/statistics.py
@@ -17,29 +17,27 @@ def statistic_for_all_departments(request):
     :param request:
     :return: json with statistic data
     """
-    quantity_of_events_by_departments = {}
-    quantity_of_students_by_departments = {}
-    average_attendance = {}
-
+    stats = {}
     for department in Department.objects.all():
         try:
-            quantity_of_events_by_departments[str(department)] = len(
-                Event.objects.filter(owner__department_id=department))
-            quantity_of_students_by_departments[str(department)] = len(
-                CustomUser.objects.filter(department_id=department))
+            data = {
+                "quantity_of_events_by_departments": len(
+                    Event.objects.filter(owner__department_id=department)),
+                "quantity_of_students_by_departments": len(
+                    CustomUser.objects.filter(department_id=department))
+            }
             # для подсчета среднего посещения мероприятий по департаментам в процентах, нужно:
             #                           (количество людей кто действительно пришел * 100(процентов))
             # средняя посещаемость(%) = ------------------------------------------------------------
             #                                        количество приглашенных людей
             polls = len(Poll.objects.filter(user__department_id=department))
             agreed_polls = len(Poll.objects.filter(user__department_id=department).filter(was_on_event=True))
-            average_attendance[str(department)] = math.ceil(agreed_polls * 100 / polls)
+            data["average_attendance"] = math.ceil(agreed_polls * 100 / polls)
         except ZeroDivisionError:
-            average_attendance[str(department)] = 0
-
-    return JsonResponse({"quantity_of_events_by_department": quantity_of_events_by_departments,
-                         "quantity_of_students_by_departments": quantity_of_students_by_departments,
-                         "average_attendance": average_attendance})
+            data["average_attendance"] = 0
+        finally:
+            stats[str(department)] = data
+    return JsonResponse(stats)
 
 
 @api_view(["GET"])
@@ -50,8 +48,9 @@ def self_statistic(request):
     :param request:
     :return: json with statistic data
     """
-    stats = {}
-    stats["quantity_of_polls"] = len(Poll.objects.filter(user=request.user))
-    stats["quantity_of_attended_events"] = len(Poll.objects.filter(user=request.user).filter(was_on_event=True))
+    stats = {
+        "quantity_of_polls": len(Poll.objects.filter(user=request.user)),
+        "quantity_of_attended_events": len(Poll.objects.filter(user=request.user).filter(was_on_event=True))
+    }
     stats["quantity_of_missed_events"] = stats["quantity_of_polls"] - stats["quantity_of_attended_events"]
-    return JsonResponse({"stats": stats})
+    return JsonResponse(stats)
