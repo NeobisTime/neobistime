67133fb017d620602ed7f6c13bdefd6479ab5a44
inbraev <ibraev.nazar@yandex.ru>
2020-08-06 19:41:55 +0600

    added department list view

--- neobistime/users/urls.py
+++ neobistime/users/urls.py
@@ -2,13 +2,13 @@ from allauth.account.views import PasswordChangeView, PasswordResetView, confirm
 from django.conf.urls import url
 from django.urls import include, path
 
-from .views import UserListView
+from .views import UserListView,DepartmentListView
 
 urlpatterns = [
     path('', UserListView.as_view(), name='list-users'),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
-
+    path('departments/', DepartmentListView.as_view(), name='list-departments'),
     url(r'^change/password/', PasswordChangeView.as_view(), ),
     url(r'^reset/password/', PasswordResetView.as_view(), ),
     url(r'^account/', include('allauth.urls')),
