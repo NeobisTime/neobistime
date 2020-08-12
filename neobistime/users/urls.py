18faf7c0eee33380fac68877165993ecc9c8c0d5
inbraev <ibraev.nazar@yandex.ru>
2020-08-12 11:42:35 +0600

    added view for password reset confirm

--- neobistime/users/urls.py
+++ neobistime/users/urls.py
@@ -1,7 +1,7 @@
 from allauth.account.views import PasswordChangeView, PasswordResetView, confirm_email
 from django.conf.urls import url
-from django.urls import include, path
-
+from django.urls import include, path, re_path
+from rest_auth.views import PasswordResetConfirmView
 from .views import UserListView, DepartmentListView
 
 urlpatterns = [
@@ -14,4 +14,8 @@ urlpatterns = [
     url(r'^account/', include('allauth.urls')),
     url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email,
         name='account_confirm_email'),
+    re_path(
+        r'^rest-auth/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
 ]
