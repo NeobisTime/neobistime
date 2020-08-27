a0cc40c142247c7e2bf22c6befd73c0bb5273855
inbraev <ibraev.nazar@yandex.ru>
2020-08-27 15:31:19 +0600

    changed view for bot

--- neobistime/events/urls.py
+++ neobistime/events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from . import statistics
 from . import views
-from . import bot
+from neobistime import bot
 
 router = DefaultRouter()
 router.register('events', views.EventViewSet, basename='events')
